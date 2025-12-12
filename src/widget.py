89bf6c8929b0f82a9c7b22269ea6453ed00741d6
Viktor Bukotin <viktorbukotin0@gmail.com>
2025-12-12 11:44:43 +0400

    add func - get_date

--- src/widget.py
+++ src/widget.py
@@ -2,25 +2,25 @@ import masks
 
 
 def mask_account_card(data_card: str) -> str:
+    """Функция, маскирующая номер карты и счета"""
     if "Счет" not in data_card:
         list_number_card = data_card.split()
-        masks_number_card = masks.get_mask_card_number(int(list_number_card[1]))
-        return f"{list_number_card[0]} {masks_number_card}"
+        masks_number_card = masks.get_mask_card_number(int(list_number_card[-1]))
+        list_number_card[-1] = masks_number_card
+        return " ".join(list_number_card)
     else:
         list_count_card = data_card.split()
-        masks_count_card = masks.get_mask_account(int(list_count_card[1]))
+        masks_count_card = masks.get_mask_account(int(list_count_card[-1]))
         return f"{list_count_card[0]} {masks_count_card}"
 
 
+def get_date(card_date: str) -> str:
+    return f"{card_date[8:10]}.{card_date[5:7]}.{card_date[:4]}"
+
+
 card_info = "Счет 64686473678894779589"
+date = "2024-03-11T02:26:18.671407"
+
 if __name__ == "__main__":
     print(mask_account_card(card_info))
-
-# Maestro 1596837868705199
-# Счет 64686473678894779589
-# MasterCard 7158300734726758
-# Счет 35383033474447895560
-# Visa Classic 6831982476737658
-# Visa Platinum 8990922113665229
-# Visa Gold 5999414228426353
-# Счет 73654108430135874305
+    get_date(date)
