d6586b7c6079840d8b38299acbb917c2fb591c5e
Viktor Bukotin <viktorbukotin0@gmail.com>
2025-12-23 20:12:00 +0400

    tests_for_get_mask_card_number_ready

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,10 @@
 def get_mask_card_number(card_number_: int) -> str:
     """Функция, возвращающая зашифрованный номер карты"""
+    if not isinstance(card_number_, int):
+        raise TypeError("Неправильный тип символов номера карты")
+    if len(str(card_number_)) != 16:
+        raise ValueError("Неправильное количество символов номера карты")
+
     first_part_number = str(card_number_)[:4]
     second_part_number = str(card_number_)[4:6]
     end_part_number = str(card_number_)[12:]
@@ -12,7 +17,7 @@ def get_mask_account(count_number_: int) -> str:
 
 
 if __name__ == "__main__":
-    card_number = 7000792289606361
+    card_number = 7000792289606363
     count_number = 73654108430135874305
     get_mask_card_number(card_number)
     get_mask_account(count_number)
