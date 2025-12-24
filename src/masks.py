81e9edd33bdaad3c8fc5a6210d8099ee75207806
Viktor Bukotin <viktorbukotin0@gmail.com>
2025-12-24 12:53:20 +0400

    tests_for_get_mask_card_number_for_str

--- src/masks.py
+++ src/masks.py
@@ -1,23 +1,23 @@
-def get_mask_card_number(card_number_: int) -> str:
+def get_mask_card_number(card_number_: str) -> str:
     """Функция, возвращающая зашифрованный номер карты"""
-    if not isinstance(card_number_, int):
-        raise TypeError("Неправильный тип символов номера карты")
-    if len(str(card_number_)) != 16:
+    if not isinstance(int(card_number_), int):
+        raise Exception("Неправильный тип символов номера карты")
+    if len(card_number_) != 16:
         raise ValueError("Неправильное количество символов номера карты")
 
-    first_part_number = str(card_number_)[:4]
-    second_part_number = str(card_number_)[4:6]
-    end_part_number = str(card_number_)[12:]
+    first_part_number = card_number_[:4]
+    second_part_number = card_number_[4:6]
+    end_part_number = card_number_[12:]
     return f"{first_part_number} {second_part_number}** **** {end_part_number}"
 
 
-def get_mask_account(count_number_: int) -> str:
+def get_mask_account(count_number_: str) -> str:
     """Функция, возвращающая зашифрованный номер счета"""
-    return f"**{str(count_number_)[-4:]}"
+    return f"**{count_number_[-4:]}"
 
 
 if __name__ == "__main__":
-    card_number = 7000792289606363
-    count_number = 73654108430135874305
+    card_number = "1000792289606363"
+    count_number = "73654108430135874305"
     get_mask_card_number(card_number)
     get_mask_account(count_number)
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,16 +3,16 @@ import pytest
 from src import masks
 
 
-@pytest.mark.parametrize("card_number, result", [(7000792289606361, "7000 79** **** 6361"),
-                                                 (9999999999999999, "9999 99** **** 9999"),
-                                                 (1000000000000000, "1000 00** **** 0000")])
+@pytest.mark.parametrize("card_number, result", [("7000792289606361", "7000 79** **** 6361"),
+                                                 ("9999999999999999", "9999 99** **** 9999"),
+                                                 ("0000000000000000", "0000 00** **** 0000")])
 def test_get_mask_card_positive(card_number, result):
     assert masks.get_mask_card_number(card_number) == result
 
 
 @pytest.fixture
 def short_number():
-    return 700079228
+    return "700079228"
 
 
 def test_get_mask_card_short_number(short_number):
@@ -22,7 +22,7 @@ def test_get_mask_card_short_number(short_number):
 
 @pytest.fixture
 def large_number():
-    return 7000792280000000000
+    return "7000792280000000001"
 
 
 def test_get_mask_card_large_number(large_number):
@@ -30,10 +30,11 @@ def test_get_mask_card_large_number(large_number):
         masks.get_mask_card_number(large_number)
 
 @pytest.mark.parametrize("card_number", ["abcdabcdabcdabcd",
-                                         9999999.99999999,
+                                         "9999999.99999999",
                                          "",
-                                         [9999999999999999, "abcdabcdabcdabcd"]])
+                                         [9999999999999999, "abcdabcdabcdabcd"]
+                                         ])
 def test_get_mask_card_not_int(card_number):
-    with pytest.raises(TypeError):
+    with pytest.raises(Exception):
         masks.get_mask_card_number(card_number)
 
