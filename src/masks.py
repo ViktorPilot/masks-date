e60c3b046c37d37e226e3e0552d18b444c696e66
Viktor Bukotin <viktorbukotin0@gmail.com>
2025-12-24 15:12:47 +0400

    tests_masks_ready

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,9 @@
 def get_mask_card_number(card_number_: str) -> str:
     """Функция, возвращающая зашифрованный номер карты"""
-    if not isinstance(int(card_number_), int):
-        raise Exception("Неправильный тип символов номера карты")
     if len(card_number_) != 16:
         raise ValueError("Неправильное количество символов номера карты")
-
+    if not isinstance(int(card_number_), int):
+        raise ValueError("Неправильный тип символов номера карты")
     first_part_number = card_number_[:4]
     second_part_number = card_number_[4:6]
     end_part_number = card_number_[12:]
@@ -13,11 +12,13 @@ def get_mask_card_number(card_number_: str) -> str:
 
 def get_mask_account(count_number_: str) -> str:
     """Функция, возвращающая зашифрованный номер счета"""
+    if len(count_number_) != 20:
+        raise ValueError("Неправильное количество символов номера счета")
+    if not isinstance(int(count_number_), int):
+        raise ValueError
     return f"**{count_number_[-4:]}"
 
 
 if __name__ == "__main__":
-    card_number = "1000792289606363"
-    count_number = "73654108430135874305"
-    get_mask_card_number(card_number)
-    get_mask_account(count_number)
+    get_mask_card_number("1000792289606363")
+    get_mask_account("73654108430135874305")
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -29,12 +29,34 @@ def test_get_mask_card_large_number(large_number):
     with pytest.raises(ValueError):
         masks.get_mask_card_number(large_number)
 
+
 @pytest.mark.parametrize("card_number", ["abcdabcdabcdabcd",
                                          "9999999.99999999",
                                          "",
-                                         [9999999999999999, "abcdabcdabcdabcd"]
+                                         [7000792289606361, "7000792289606362"]
                                          ])
 def test_get_mask_card_not_int(card_number):
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         masks.get_mask_card_number(card_number)
 
+@pytest. mark.parametrize("count_number, result", [("12345678901234504305", "**4305"),
+                                                   ("00000000000000000000", "**0000"),
+                                                   ("99999999999999999999", "**9999"),
+                                                   ])
+def test_get_mask_account_positive(count_number, result):
+    assert masks.get_mask_account(count_number) == result
+
+@pytest.mark.parametrize("count_number", ["736541084",
+                                          "7365410843013587430500000001"])
+def test_get_mask_account_short_and_large(count_number):
+    with pytest.raises(ValueError):
+        masks.get_mask_account(count_number)
+
+@pytest.mark.parametrize("count_number", ["abcdabcdabcdabcdabcd",
+                                         "9999999.999999999999",
+                                         "",
+                                         [99999999999999999999, "abcdabcdabcdabcdabc"]
+                                         ])
+def test_get_mask_account_not_int(count_number):
+    with pytest.raises(ValueError):
+        masks.get_mask_card_number(count_number)
