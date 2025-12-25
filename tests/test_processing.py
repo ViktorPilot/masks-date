ce69e5631c692867ccb59562962dc5694da69b43
Viktor Bukotin <viktorbukotin0@gmail.com>
2025-12-25 15:12:00 +0400

    test_and_processing_after_mypy

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,44 +4,48 @@ from src import processing
 
 
 @pytest.fixture
-def filter_by_state_positive():
+def filter_by_state_positive() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     ]
 
 
-def test_filter_by_state_positive_executed(filter_by_state_positive):
+def test_filter_by_state_positive_executed(filter_by_state_positive: list[dict]) -> None:
+    """Проверка работы функции, возвращающей список словарей с заданным статусом 'EXECUTED'"""
     assert processing.filter_by_state(filter_by_state_positive) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]
 
 
-def test_filter_by_state_positive_canceled(filter_by_state_positive):
+def test_filter_by_state_positive_canceled(filter_by_state_positive: list[dict]) -> None:
+    """Проверка работы функции, возвращающей список словарей с заданным статусом 'CANCELED'"""
     assert processing.filter_by_state(filter_by_state_positive, state="CANCELED") == [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}]
 
 
 @pytest.fixture
-def filter_by_state_not_executed():
+def filter_by_state_not_executed() -> list[dict]:
     return [
         {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}
     ]
 
 
-def test_filter_by_state_not_executed(filter_by_state_not_executed):
+def test_filter_by_state_not_executed(filter_by_state_not_executed: list[dict]) -> None:
+    """Проверка работы функции, не содержащей значений с заданным статусом 'EXECUTED'"""
     assert processing.filter_by_state(filter_by_state_not_executed) == []
 
 
 @pytest.fixture
-def filter_by_state_not_canceled():
+def filter_by_state_not_canceled() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"}
     ]
 
 
-def test_filter_by_state_not_canceled(filter_by_state_not_canceled):
+def test_filter_by_state_not_canceled(filter_by_state_not_canceled: list[dict]) -> None:
+    """Проверка работы функции, не содержащей значений с заданным статусом 'CANCELED'"""
     assert processing.filter_by_state(filter_by_state_not_canceled, state="CANCELED") == []
 
 
@@ -50,7 +54,8 @@ def test_filter_by_state_not_canceled(filter_by_state_not_canceled):
                             {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
                             {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],
                            [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}])])
-def test_filter_by_state_not_state_executed(list_of_dict, result):
+def test_filter_by_state_not_state_executed(list_of_dict: list[dict], result: list[dict]) -> None:
+    """Проверка работы функции с заданным статусом 'EXECUTED' при отсутствии в одном из словарей ключа 'state' """
     assert processing.filter_by_state(list_of_dict) == result
 
 
@@ -59,12 +64,13 @@ def test_filter_by_state_not_state_executed(list_of_dict, result):
                             {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
                             {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],
                            [{"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}])])
-def test_filter_by_state_not_state_canceled(list_of_dict, result):
+def test_filter_by_state_not_state_canceled(list_of_dict: list[dict], result: list[dict]) -> None:
+    """Проверка работы функции с заданным статусом 'CANCELED' при отсутствии в одном из словарей ключа 'state' """
     assert processing.filter_by_state(list_of_dict, state="CANCELED") == result
 
 
 @pytest.fixture
-def sort_by_date_positive():
+def sort_by_date_positive() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -73,7 +79,8 @@ def sort_by_date_positive():
     ]
 
 
-def test_sort_by_date_positive_reduce(sort_by_date_positive):
+def test_sort_by_date_positive_reduce(sort_by_date_positive: list[dict]) -> None:
+    """Проверка работы функции, возвращающая список словарей отсортированных по уменьшению значений даты"""
     assert processing.sort_by_date(sort_by_date_positive) == [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
@@ -81,7 +88,8 @@ def test_sort_by_date_positive_reduce(sort_by_date_positive):
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 
-def test_sort_by_date_positive_increase(sort_by_date_positive):
+def test_sort_by_date_positive_increase(sort_by_date_positive: list[dict]) -> None:
+    """Проверка работы функции, возвращающая список словарей отсортированных по возрастанию значений даты"""
     assert processing.sort_by_date(sort_by_date_positive, type_sort=False) == [
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -90,7 +98,7 @@ def test_sort_by_date_positive_increase(sort_by_date_positive):
 
 
 @pytest.fixture
-def sort_by_date_equal_date():
+def sort_by_date_equal_date() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T01:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T02:08:58.425572"},
@@ -99,7 +107,9 @@ def sort_by_date_equal_date():
     ]
 
 
-def test_sort_by_date_equal_date_reduce(sort_by_date_equal_date):
+def test_sort_by_date_equal_date_reduce(sort_by_date_equal_date: list[dict]) -> None:
+    """Проверка работы функции, возвращающая список словарей при одинаковых
+    значениях даты с типом сортировки по умолчанию"""
     assert processing.sort_by_date(sort_by_date_equal_date) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T01:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T02:08:58.425572"},
@@ -108,7 +118,9 @@ def test_sort_by_date_equal_date_reduce(sort_by_date_equal_date):
     ]
 
 
-def test_sort_by_date_equal_date_increase(sort_by_date_equal_date):
+def test_sort_by_date_equal_date_increase(sort_by_date_equal_date: list[dict]) -> None:
+    """Проверка работы функции, возвращающая список словарей при одинаковых
+        значениях даты с типом сортировки на возрастание значений даты"""
     assert processing.sort_by_date(sort_by_date_equal_date, type_sort=False) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T01:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T02:08:58.425572"},
@@ -123,6 +135,7 @@ def test_sort_by_date_equal_date_increase(sort_by_date_equal_date):
                                           [{"id": 41428829, "state": "EXECUTED", "date": "2019-12-03T18:35:29.512364"},
                                            {"id": 939719570, "state": "EXECUTED",
                                             "date": "2018-06-32T02:08:58.425572"}]])
-def test_sort_by_date_failed_date(list_of_dict):
+def test_sort_by_date_failed_date(list_of_dict: list[dict]) -> None:
+    """Проверка работы функции, вызывающей исключение при недопустимых значениях даты"""
     with pytest.raises(ValueError):
         processing.sort_by_date(list_of_dict)
