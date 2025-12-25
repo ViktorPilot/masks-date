0d1fea0694263c7e8880fdef0e33c843008ada65
Viktor Bukotin <viktorbukotin0@gmail.com>
2025-12-25 16:03:09 +0400

    all_after_mypy_isort_black

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,9 +3,14 @@ import pytest
 from src import masks
 
 
-@pytest.mark.parametrize("card_number, result", [("7000792289606361", "7000 79** **** 6361"),
-                                                 ("9999999999999999", "9999 99** **** 9999"),
-                                                 ("0000000000000000", "0000 00** **** 0000")])
+@pytest.mark.parametrize(
+    "card_number, result",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("9999999999999999", "9999 99** **** 9999"),
+        ("0000000000000000", "0000 00** **** 0000"),
+    ],
+)
 def test_get_mask_card_positive(card_number: str, result: str) -> None:
     """Проверка работы функции, возвращающей зашифрованный номер карты при стандартных значениях"""
     assert masks.get_mask_card_number(card_number) == result
@@ -33,41 +38,42 @@ def test_get_mask_card_large_number(large_number: str) -> None:
         masks.get_mask_card_number(large_number)
 
 
-@pytest.mark.parametrize("card_number", ["abcdabcdabcdabcd",
-                                         "9999999.99999999",
-                                         "",
-                                         [7000792289606361, "7000792289606362"]
-                                         ])
+@pytest.mark.parametrize(
+    "card_number", ["abcdabcdabcdabcd", "9999999.99999999", "", [7000792289606361, "7000792289606362"]]
+)
 def test_get_mask_card_not_int(card_number: str) -> None:
-    """Проверка работы функции, вызывающей исключение при вводе номера карты со значениями, отличными от целого числа"""
+    """Проверка работы функции, вызывающей исключение при вводе номера карты со значениями,
+    отличными от целого числа"""
     with pytest.raises(ValueError):
         masks.get_mask_card_number(card_number)
 
 
-@pytest.mark.parametrize("count_number, result", [("12345678901234504305", "**4305"),
-                                                  ("00000000000000000000", "**0000"),
-                                                  ("99999999999999999999", "**9999"),
-                                                  ])
+@pytest.mark.parametrize(
+    "count_number, result",
+    [
+        ("12345678901234504305", "**4305"),
+        ("00000000000000000000", "**0000"),
+        ("99999999999999999999", "**9999"),
+    ],
+)
 def test_get_mask_account_positive(count_number: str, result: str) -> None:
     """Проверка работы функции, возвращающей зашифрованный номер счета при стандартных значениях"""
     assert masks.get_mask_account(count_number) == result
 
 
-@pytest.mark.parametrize("count_number", ["736541084",
-                                          "7365410843013587430500000001"])
+@pytest.mark.parametrize("count_number", ["736541084", "7365410843013587430500000001"])
 def test_get_mask_account_short_and_large(count_number: str) -> None:
     """Проверка работы функции, вызывающей исключение при вводе номера счета
-     с количеством символов отличным от заданного"""
+    с количеством символов отличным от заданного"""
     with pytest.raises(ValueError):
         masks.get_mask_account(count_number)
 
 
-@pytest.mark.parametrize("count_number", ["abcdabcdabcdabcdabcd",
-                                          "9999999.999999999999",
-                                          "",
-                                          [99999999999999999999, "abcdabcdabcdabcdabc"]
-                                          ])
+@pytest.mark.parametrize(
+    "count_number", ["abcdabcdabcdabcdabcd", "9999999.999999999999", "", [99999999999999999999, "abcdabcdabcdabcdabc"]]
+)
 def test_get_mask_account_not_int(count_number: str) -> None:
-    """Проверка работы функции, вызывающей исключение при вводе номера счета со значениями, отличными от целого числа"""
+    """Проверка работы функции, вызывающей исключение при вводе номера счета со значениями,
+    отличными от целого числа"""
     with pytest.raises(ValueError):
         masks.get_mask_card_number(count_number)
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -14,20 +14,22 @@ def filter_by_state_positive() -> list[dict]:
 def test_filter_by_state_positive_executed(filter_by_state_positive: list[dict]) -> None:
     """Проверка работы функции, возвращающей список словарей с заданным статусом 'EXECUTED'"""
     assert processing.filter_by_state(filter_by_state_positive) == [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
+    ]
 
 
 def test_filter_by_state_positive_canceled(filter_by_state_positive: list[dict]) -> None:
     """Проверка работы функции, возвращающей список словарей с заданным статусом 'CANCELED'"""
     assert processing.filter_by_state(filter_by_state_positive, state="CANCELED") == [
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}]
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}
+    ]
 
 
 @pytest.fixture
 def filter_by_state_not_executed() -> list[dict]:
     return [
         {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     ]
 
 
@@ -40,7 +42,7 @@ def test_filter_by_state_not_executed(filter_by_state_not_executed: list[dict]) 
 def filter_by_state_not_canceled() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"}
+        {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
     ]
 
 
@@ -49,23 +51,39 @@ def test_filter_by_state_not_canceled(filter_by_state_not_canceled: list[dict]) 
     assert processing.filter_by_state(filter_by_state_not_canceled, state="CANCELED") == []
 
 
-@pytest.mark.parametrize("list_of_dict, result",
-                         [([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                            {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
-                            {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],
-                           [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}])])
+@pytest.mark.parametrize(
+    "list_of_dict, result",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+                {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+            [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}],
+        )
+    ],
+)
 def test_filter_by_state_not_state_executed(list_of_dict: list[dict], result: list[dict]) -> None:
-    """Проверка работы функции с заданным статусом 'EXECUTED' при отсутствии в одном из словарей ключа 'state' """
+    """Проверка работы функции с заданным статусом 'EXECUTED' при отсутствии в одном из словарей ключа 'state'"""
     assert processing.filter_by_state(list_of_dict) == result
 
 
-@pytest.mark.parametrize("list_of_dict, result",
-                         [([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                            {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
-                            {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],
-                           [{"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}])])
+@pytest.mark.parametrize(
+    "list_of_dict, result",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+                {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+            [{"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],
+        )
+    ],
+)
 def test_filter_by_state_not_state_canceled(list_of_dict: list[dict], result: list[dict]) -> None:
-    """Проверка работы функции с заданным статусом 'CANCELED' при отсутствии в одном из словарей ключа 'state' """
+    """Проверка работы функции с заданным статусом 'CANCELED' при отсутствии в одном из словарей ключа 'state'"""
     assert processing.filter_by_state(list_of_dict, state="CANCELED") == result
 
 
@@ -82,19 +100,21 @@ def sort_by_date_positive() -> list[dict]:
 def test_sort_by_date_positive_reduce(sort_by_date_positive: list[dict]) -> None:
     """Проверка работы функции, возвращающая список словарей отсортированных по уменьшению значений даты"""
     assert processing.sort_by_date(sort_by_date_positive) == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
 def test_sort_by_date_positive_increase(sort_by_date_positive: list[dict]) -> None:
     """Проверка работы функции, возвращающая список словарей отсортированных по возрастанию значений даты"""
     assert processing.sort_by_date(sort_by_date_positive, type_sort=False) == [
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
 
 
 @pytest.fixture
@@ -120,7 +140,7 @@ def test_sort_by_date_equal_date_reduce(sort_by_date_equal_date: list[dict]) -> 
 
 def test_sort_by_date_equal_date_increase(sort_by_date_equal_date: list[dict]) -> None:
     """Проверка работы функции, возвращающая список словарей при одинаковых
-        значениях даты с типом сортировки на возрастание значений даты"""
+    значениях даты с типом сортировки на возрастание значений даты"""
     assert processing.sort_by_date(sort_by_date_equal_date, type_sort=False) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T01:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T02:08:58.425572"},
@@ -129,12 +149,19 @@ def test_sort_by_date_equal_date_increase(sort_by_date_equal_date: list[dict]) -
     ]
 
 
-@pytest.mark.parametrize("list_of_dict", [[{"id": 41428829, "state": "EXECUTED", "date": "2019-13-03T18:35:29.512364"},
-                                           {"id": 939719570, "state": "EXECUTED",
-                                            "date": "2018-06-31T02:08:58.425572"}],
-                                          [{"id": 41428829, "state": "EXECUTED", "date": "2019-12-03T18:35:29.512364"},
-                                           {"id": 939719570, "state": "EXECUTED",
-                                            "date": "2018-06-32T02:08:58.425572"}]])
+@pytest.mark.parametrize(
+    "list_of_dict",
+    [
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-13-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-31T02:08:58.425572"},
+        ],
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-12-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-32T02:08:58.425572"},
+        ],
+    ],
+)
 def test_sort_by_date_failed_date(list_of_dict: list[dict]) -> None:
     """Проверка работы функции, вызывающей исключение при недопустимых значениях даты"""
     with pytest.raises(ValueError):
