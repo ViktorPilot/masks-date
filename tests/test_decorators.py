0b5b4d012f01e6cf709690ecd6057beb4666a003
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-01-14 20:59:29 +0300

    tests_decorators_is_ready

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -16,19 +16,24 @@ def my_function(x: Any, y: Any) -> Any:
 
 
 @pytest.mark.parametrize("x, y, result", [(1, 2, "my_function ok\n"), (1.2, 2.3, "my_function ok\n")])
-def test_log_positive(capsys, x, y, result):
+def test_log_positive(capsys: pytest.CaptureFixture, x: int | float, y: int | float, result: str) -> None:
     """Тестирование работы декоратора при вводе целых чисел и чисел с плавающей запятой с выводом лога в консоль"""
     my_function(x, y)
     captured = capsys.readouterr()
     assert captured.out == result
 
 
-@pytest.mark.parametrize("x, y, result", [("1", 2, "my_function error: TypeError. Inputs: ('1', 2), {}\n"),
-                                          (1, "2", "my_function error: TypeError. Inputs: (1, '2'), {}\n"),
-                                          ([1, 2], (2, 1),
-                                           "my_function error: TypeError. Inputs: ([1, 2], (2, 1)), {}\n")])
-def test_log_negative(capsys, x, y, result):
-    """Тестирование работы декоратора при вводе аргументов, отличных от целых чисел и чисел с плавающей запятой с выводом лога в консоль"""
+@pytest.mark.parametrize(
+    "x, y, result",
+    [
+        ("1", 2, "my_function error: TypeError. Inputs: ('1', 2), {}\n"),
+        (1, "2", "my_function error: TypeError. Inputs: (1, '2'), {}\n"),
+        ([1, 2], (2, 1), "my_function error: TypeError. Inputs: ([1, 2], (2, 1)), {}\n"),
+    ],
+)
+def test_log_negative(capsys: pytest.CaptureFixture, x: Any, y: Any, result: str) -> None:
+    """Тестирование работы декоратора при вводе аргументов, отличных от целых чисел
+    и чисел с плавающей запятой с выводом лога в консоль"""
     my_function(x, y)
     captured = capsys.readouterr()
     assert captured.out == result
@@ -41,13 +46,34 @@ def my_function_2(x: Any, y: Any) -> Any:
 
 
 @pytest.mark.parametrize("x, y, result", [(1, 2, "my_function_2 ok\n"), (1.2, 2.3, "my_function_2 ok\n")])
-def test_log_positive_to_mylog(x, y, result):
-    """Тестирование работы декоратора при вводе целых чисел и чисел с плавающей запятой с выводом лога в файл mylog.txt"""
+def test_log_positive_to_mylog(x: int | float, y: int | float, result: str) -> None:
+    """Тестирование работы декоратора при вводе целых чисел и чисел с плавающей запятой
+    с выводом лога в файл mylog.txt"""
     my_function_2(x, y)
     with open(path_to_mylog, "r", encoding="UTF-8") as file:
         log_in_mylog = file.readlines()
-        last_str = log_in_mylog[-1]
-    assert last_str == result
+    assert log_in_mylog[-1] == result
+
+    log_in_mylog.pop()
+    with open(path_to_mylog, "w", encoding="UTF-8") as file:
+        file.writelines(log_in_mylog)
+
+
+@pytest.mark.parametrize(
+    "x, y, result",
+    [
+        ("1", 2, "my_function_2 error: TypeError. Inputs: ('1', 2), {}\n"),
+        (1, "2", "my_function_2 error: TypeError. Inputs: (1, '2'), {}\n"),
+        ([1, 2], (2, 1), "my_function_2 error: TypeError. Inputs: ([1, 2], (2, 1)), {}\n"),
+    ],
+)
+def test_log_negative_to_mylog(x: Any, y: Any, result: str) -> None:
+    """Тестирование работы декоратора при вводе аргументов, отличных от целых чисел
+    и чисел с плавающей запятой с выводом лога в файл mylog.txt"""
+    my_function_2(x, y)
+    with open(path_to_mylog, "r", encoding="UTF-8") as file:
+        log_in_mylog = file.readlines()
+    assert log_in_mylog[-1] == result
 
     log_in_mylog.pop()
     with open(path_to_mylog, "w", encoding="UTF-8") as file:
