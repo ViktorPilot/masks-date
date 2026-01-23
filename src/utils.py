0f6a1be6513171390b78e7a96950c28936676bec
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-01-23 13:11:51 +0300

    utils_is_ready

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def get_dict_transactions(path_to_operations: str) -> list[None | dict]:
+    """Функция, преобразующая json-файл с транзакциями в список"""
+    try:
+        with open(path_to_operations, "r", encoding="utf-8") as file:
+            list_operations = json.load(file)
+            if type(list_operations) is not list:
+                list_operations = []
+    except (json.JSONDecodeError, FileNotFoundError):
+        list_operations = []
+
+    return list_operations
+
+
+if __name__ == "__main__":
+    print(get_dict_transactions(os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/operations.json"))))
